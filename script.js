83a0b58c129fd951bb67fd2f37308aef3f3cb96d
suptohossein <supto.hossein@gmail.com>
2023-06-21 23:08:49 +0600

    update as per assignment requirement

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getWeather() {
+    // get value form input box
+    const currentLocation = document.querySelector("#location").value;
+
+    // base api url
+    const base_url = "https://api.openweathermap.org/data/2.5/weather?";
+    
+    // api key
+    const key = "3fd55917ebac1a4a2cb8195f3f5d44bd";
+
+    // fetch api
+    const response = await fetch(`${base_url}units=metric&q=${currentLocation}&appid=${key}`);
+    const data = await response.json();
+    document.querySelector(".tempareture").innerHTML = Math.round(data.main.temp) + `°c`;
+    console.log(data);
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    document.querySelector(
+      ".date"
+    ).innerHTML = `Today is: <b>${day}/${month}/${year}</b>`;
+    document.querySelector(".location").innerHTML = `Location is: <b>${data.name}</b>`;
+  }
