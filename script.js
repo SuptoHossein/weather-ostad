20f454d9a321204192baad2f15285a013f3badfe
suptohossein <supto.hossein@gmail.com>
2023-07-06 12:08:28 +0600

    error hendling for empty string on script.js file.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 async function getWeather() {
-    // get value form input box
-    const currentLocation = document.querySelector("#location").value;
+  // get value form input box
+  const currentLocation = document.querySelector("#location").value;
 
-    // base api url
-    const base_url = "https://api.openweathermap.org/data/2.5/weather?";
-    
-    // api key
-    const key = "3fd55917ebac1a4a2cb8195f3f5d44bd";
+  // base api url
+  const base_url = "https://api.openweathermap.org/data/2.5/weather?";
 
-    // fetch api
+  // api key
+  const key = "3fd55917ebac1a4a2cb8195f3f5d44bd";
+
+  // fetch api
+  if (!currentLocation == '') {
     const response = await fetch(`${base_url}units=metric&q=${currentLocation}&appid=${key}`);
     const data = await response.json();
-    
+
     document.querySelector(".tempareture").innerHTML = Math.round(data.main.temp) + `°c`;
+
 
     const date = new Date();
     const year = date.getFullYear();
@@ -21,4 +23,8 @@ async function getWeather() {
     document.querySelector(".date").innerHTML = `Today is: <b>${day}/${month}/${year}</b>`;
     document.querySelector(".location").innerHTML = `Location is: <b>${data.name}</b>`;
     document.querySelector(".humidity").innerHTML = `Humidity: <b>${data.main.humidity}%</b>`;
+  } else {
+    alert('Please enter location first!')
   }
+
+}
